fe5be686c6133010c97490144b63de1d2f562123
Aung-Zay-Thant <aungzaythant.csdt@gmail.com>
2020-08-17 13:34:46 +0630

    Sixth Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,80 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(MaterialApp(
+    home: CategoryScreen(),
+    debugShowCheckedModeBanner: false,
+  ));
 }
 
-class MyApp extends StatelessWidget {
+class CategoryScreen extends StatefulWidget {
+  @override
+  _CategoryScreenState createState() => _CategoryScreenState();
+}
+
+class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("sqflite"),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          scrollDirection: Axis.horizontal,
+          child: SingleChildScrollView(
+            scrollDirection: Axis.vertical,
+            child: DataTable(
+              columns: [
+                DataColumn(
+                    label: Text('Hello',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18))),
+                DataColumn(
+                    label: Text('Hello',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18))),
+                DataColumn(
+                    label: Text('Hello',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 18))),
+              ],
+              rows: [
+                DataRow(cells: [
+                  DataCell(
+                    Text('Hello',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500)),
+                  ),
+                  DataCell(
+                    Text('Hello',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500)),
+                  ),
+                  DataCell(
+                    Text('Hello',
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500)),
+                  ),
+                ])
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.teal,
+            child: Icon(Icons.add),
+            onPressed: () {}),
       ),
-      body: Center(),
     );
   }
 }
